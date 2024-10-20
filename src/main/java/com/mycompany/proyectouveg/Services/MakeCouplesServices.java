cd2227962308b00aae5dc177be6904a1577f6138
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2024-10-19 18:40:16 -0600

    feat: add methods to class MakCouplesServices for sort and verify that users are sorted before to make cuples

--- src/main/java/com/mycompany/proyectouveg/Services/MakeCouplesServices.java
+++ src/main/java/com/mycompany/proyectouveg/Services/MakeCouplesServices.java
@@ -1,4 +1,34 @@
 package com.mycompany.proyectouveg.Services;
+import com.mycompany.proyectouveg.Students.Gender;
+import com.mycompany.proyectouveg.Students.User;
+import com.mycompany.proyectouveg.enums.*;
 
-public class MakeCouples {
+import java.util.ArrayList;
+
+public class MakeCouplesServices {
+    private static ArrayList<User> womans = new ArrayList<>();
+    private static ArrayList<User> mans = new ArrayList<>();
+    private  boolean is_order = true;
+    static User searchCoupleForUserId(User[] users, int idUser) {
+        return users[0];
+    };
+    void sortUsersByGender(User[] users){
+        if(this.is_order == true){
+            System.out.println("Not Order the users are ordered");
+            return;
+        };
+        int length_users = users.length - 1;
+        while(length_users >= 0){
+            if(users[length_users].getGender() == Gender.Femenino){
+                womans.add(users[length_users]);
+            }else mans.add(users[length_users]);
+            length_users--;
+        }
+    }
+    boolean getOrder(){
+        return this.is_order;
+    }
+    void setOrder(boolean is_order){
+        this.is_order = is_order;
+    }
 }
