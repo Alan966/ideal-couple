2b9947a87a1695b034bd866975ac75939f0dea6f
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2024-10-19 19:20:47 -0600

    perf: ony order the latest users that haven added before the last loop

--- src/main/java/com/mycompany/proyectouveg/Services/MakeCouplesServices.java
+++ src/main/java/com/mycompany/proyectouveg/Services/MakeCouplesServices.java
@@ -8,18 +8,21 @@ import java.util.ArrayList;
 public class MakeCouplesServices {
     private static ArrayList<User> womans = new ArrayList<>();
     private static ArrayList<User> mans = new ArrayList<>();
+    private int orders = 0;
     private  boolean is_order = true;
     private ArrayList<User> users = DB.getUsers();
-    public User searchCoupleForUserId(User[] users, int idUser) {
-        return users[0];
+    public User searchCoupleForUserId( int idUser) {
+       return users.get(1);
     };
     public void sortUsersByGender(){
-        if(this.is_order == true){
+        if(this.is_order){
             System.out.println("Not Order the users are ordered");
             return;
         };
         int length_users = users.size() - 1;
-        while(length_users >= 0){
+        int orders_in_the_last_loop = this.orders;
+        while(length_users >= orders_in_the_last_loop){
+            this.orders++;
             User index_user = users.get(length_users);
             if(index_user.getGender() == Gender.Femenino){
                 womans.add(index_user);
@@ -34,4 +37,5 @@ public class MakeCouplesServices {
     public void setOrder(boolean is_order){
         this.is_order = is_order;
     }
+
 }
