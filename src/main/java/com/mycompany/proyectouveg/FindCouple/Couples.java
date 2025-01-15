fe2b60b5602b50e0be2f147547b2eeda7a7856e9
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2025-01-14 19:08:15 -0600

    add: static to methods that should be static

--- src/main/java/com/mycompany/proyectouveg/FindCouple/Couples.java
+++ src/main/java/com/mycompany/proyectouveg/FindCouple/Couples.java
@@ -7,30 +7,38 @@ import java.util.GregorianCalendar;
 
 public abstract class Couples {
     public final User user;
-    public Couples(User _user){
+
+    public Couples(User _user) {
         this.user = _user;
     }
-    public void executeQuestions(){
+
+    public void executeQuestions() {
         FirstQuestion();
         SecondQuestion();
         ThirdQuestion();
         FourthQuestion();
         FifthQuestion();
     }
+
     public abstract void start();
-    public boolean couldPlay(){
+
+    public boolean couldPlay() {
         Calendar now = new GregorianCalendar();
         int difference = this.user.getBirthday().getYear() - now.getTime().getYear();
-        if (difference > 18 || difference < -18 ) {
+        if (difference > 18 || difference < -18) {
             return true;
         }
         System.out.println("Cierra la computadora");
         return false;
-    };
-    public boolean validateResponse(String response){
-        return  response.equals("a") || response.equals("b") || response.equals("c") || response.equals("d");
     }
-    public int returnPoints(String response){
+
+    ;
+
+    public static boolean validateResponse(String response) {
+        return response.equals("a") || response.equals("b") || response.equals("c") || response.equals("d");
+    }
+
+    public static int returnPoints(String response) {
         switch (response) {
             case "a":
                 return 4;
@@ -39,15 +47,21 @@ public abstract class Couples {
             case "c":
                 return 2;
             case "d":
-                return  1;
+                return 1;
             default:
                 return 0;
         }
     }
+
     protected abstract void FirstQuestion();
-    protected abstract  void SecondQuestion();
+
+    protected abstract void SecondQuestion();
+
     protected abstract void ThirdQuestion();
-    protected abstract  void FourthQuestion();
+
+    protected abstract void FourthQuestion();
+
     protected abstract void FifthQuestion();
-    public  abstract String[] getAdvices();
+
+    public abstract String[] getAdvices();
 }
