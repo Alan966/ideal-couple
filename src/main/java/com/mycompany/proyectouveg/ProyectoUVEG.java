965171e34444c26d4b90c92d4520b9ecede503a2
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2025-01-14 20:04:54 -0600

    refact: execute the program thought try catch finally

--- src/main/java/com/mycompany/proyectouveg/ProyectoUVEG.java
+++ src/main/java/com/mycompany/proyectouveg/ProyectoUVEG.java
@@ -5,6 +5,7 @@ package com.mycompany.proyectouveg;
 
 import com.mycompany.proyectouveg.Users.Gender;
 import com.mycompany.proyectouveg.Users.User;
+
 import java.util.Scanner;
 
 /**
@@ -12,32 +13,40 @@ import java.util.Scanner;
  */
 public class ProyectoUVEG {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        User user = new User(sc);
-        user.start();
-        getUserInfo(user);
-        String [] [] advices = user.getAdvices();
-        for (String[] advice : advices) {
-            printAdvices(advice);
+        try {
+            Scanner sc = new Scanner(System.in);
+            User user = new User(sc);
+            user.start();
+            getUserInfo(user);
+            String[][] advices = user.getAdvices();
+            for (String[] advice : advices) {
+                printAdvices(advice);
+            }
+            if (!user.isPremium()) {
+                System.out.println("Do you want to become premium ?");
+                System.out.println("Answer false you don't want to become premium");
+                String answer = sc.nextLine().trim().toLowerCase();
+                if (answer.equals("false")) return;
+                System.out.println("Please insert the amount ");
+                int amount = Integer.parseInt(sc.nextLine());
+                user.setAmount(amount);
+            }
+            user.maybeStartProcessPremium(sc);
+        } catch (Exception e) {
+            System.out.println("Has an error this code: " + e);
+        } finally {
+            System.out.println("Thanks to play");
         }
-        if(!user.isPremium()){
-            System.out.println("Do you want to become premium ?");
-            System.out.println("Answer false you don't want to become premium");
-            String answer = sc.nextLine().trim().toLowerCase();
-            if (answer.equals("false")) return;
-            System.out.println("Please insert the amount ");
-            int amount = Integer.parseInt(sc.nextLine());
-            user.setAmount(amount);
-        }
-        user.maybeStartProcessPremium(sc);
     }
-    private static void  getUserInfo(User user){
+
+    private static void getUserInfo(User user) {
         System.out.println("This is your id: " + user.getUserId());
         System.out.println("This is your Emotional partner result:" + user.getEmotionalPartner());
         System.out.println("This is your Life and Goals partner result : " + user.getLifeAndAmbitions());
         System.out.println("This is your Love Lenguage And Affection partner result : " + user.getLoveLenguagePartner());
         System.out.println("This is your Life and Social Preferences partner result: " + user.getLifeAndSocialPreferencesCouple());
     }
+
     private static void printAdvices(String[] advices) {
         int length_advice = advices.length;
         int i = 0;
